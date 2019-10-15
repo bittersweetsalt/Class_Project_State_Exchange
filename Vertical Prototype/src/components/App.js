4b99950c416165a3df9977403946b869629d7299
Chris Jol <jol.chris2@gmail.com>
2019-10-15 14:47:11 -0700

    Added axios network request functionality

--- Vertical Prototype/src/components/App.js
+++ Vertical Prototype/src/components/App.js
@@ -1,9 +1,11 @@
-import './App.css';
 import React from 'react';
+import axios from 'axios';
+import './App.css';
 import List from "./List";
 
 class App extends React.Component {
     state = {
+        query: "",
         queryData: [
             {
                 name: "X-box",
@@ -16,6 +18,16 @@ class App extends React.Component {
         ]
     }
 
+    onQuerySubmit = async event => {
+        event.preventDefault();
+        
+        const response = await axios.get('http://ec2-52-14-195-91.us-east-2.compute.amazonaws.com:3000/',{
+            query: this.state.query
+        });
+
+        this.setState({ queryData: response});
+    }
+
     render() {
         return (
             <div className="app">
@@ -24,8 +36,13 @@ class App extends React.Component {
                 </div>
                 <h2>State Exchange</h2>
                 <form>
-                    <input type="text" placeholder="X-box, Macbook"></input>
-                    <button>Search</button>
+                    <input 
+                        type="text"
+                        value={this.state.query}
+                        placeholder="X-box, Macbook"
+                        onChange = { e => this.setState({ query: e.target.value})}
+                    ></input>
+                    <button onSubmit={ e => this.onQuerySubmit(e)}>Search</button>
                 </form>
                 <List data = {this.state.queryData}/>
             </div>
