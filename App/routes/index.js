f0694e2cbf7cd58dbc694dbacf12487378bb76d9
Chris Jol <jol.chris2@gmail.com>
2019-11-09 18:37:58 -0800

    bugfix

--- App/routes/index.js
+++ App/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
+router.get('/index', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
 module.exports = router;
