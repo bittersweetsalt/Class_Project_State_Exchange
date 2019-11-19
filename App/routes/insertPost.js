a660108ffec5144f3bb6fbf2825f2270e579eb1b
Kevin Venegas <kevinv234@gmail.com>
2019-11-18 16:39:55 -0800

    Added posting feature, post goes directily into the database

--- App/routes/insertPost.js
+++ App/routes/insertPost.js
@@ -3,47 +3,49 @@ var app = express();
 var router = express.Router();
 const path = require('path');
 var db = require("../db/connection");
+const axios = require('axios');
 
 // we are going to generate the ids of the post  by passing parameters here in the future
 
-let id = 5;
-router.post(`/insertPost/id:${id}` ,(req, res) => {
 
-    // this is the data we are going to be sending 
-    let data = { 
-        Title: req.body.Title, 
-        Category: req.body.Category,
-        Price: req.body.price,
-        Desc: req.body.desc
+router.post(`/newpost/12` ,(req, res) => {
+
         
-    }
+        // this dummy data we send
+        let data = { 
+                              
+            Title: req.body.Title,
+            category: req.body.category,
+            UserID: 91928395,
+            Desc: req.body.Comment,
+            Price: req.body.Price,
+        }
+  
 
-    let sql = "INSERT INTO Posting SET ?";
-
-
-    db.getConnection((err, connection) => {
-
-        db.query(sql, data, (err, res, fields) => {
-            console.log(req.query.query);
-
-            if(error){
-                console.log(error);
+        let sql = "INSERT INTO Posting SET ?";
+        
+        db.query(sql, [data], (err, results) => {
+            
+            if (err){
+                console.log("Failed: " + err);
+                res.end();
+                return;
             }
-            else{
-                res.status(201).send(`Post added with ID: ${res.ID}`);
-                res.redirect('/newPosting.html');
-            }
-           
-        })
-       
-    })
-})
+              
+            console.log('Found database.... 1 Record inserted');
+        
+                // A simple "Your item has been submitted" Response Page.
+                // Setup Date: 11/14/2019
+            res.send(rows);
+            })
 
+  })    
+        
 
 //grabbing th endpoint created by vue 
-app.post('', function(req,res){
-    console.log(req.body);
-    res.send('POST request received successfully');
-})
+// app.post('', function(req,res){
+//     console.log(req.body);
+//     res.send('POST request received successfully');
+// })
 
 module.exports = router;
