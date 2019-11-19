ff4fb63f2050b29fa16b00f91f6389eca31f3d6e
Chris Jol <jol.chris2@gmail.com>
2019-11-18 17:38:20 -0800

    fixed error from previous commit, mistyped syntax in vue code

--- App/app.js
+++ App/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-var cors = require('cors');
+// var cors = require('cors');
 var db = require("./db/connection");
 
 //routes import
