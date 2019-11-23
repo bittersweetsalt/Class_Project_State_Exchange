859f45910aa2a3b5e071efa4097c2f91bc687c17
Chris Jol <jol.chris2@gmail.com>
2019-11-23 12:07:23 -0800

    keeping local changes

--- App/db/connection.js
+++ App/db/connection.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql');
 
+// const db = mysql.createConnection({
+//     user: 'ubuntu',
+//     password: 'csc648.07',
+//     database: 'sfsuDatabase',
+//     host: 'localhost',
+//     port: '3306'
+// })
+
 const db = mysql.createConnection({
-    user: 'ubuntu',
-    password: 'csc648.07',
-    database: 'sfsuDatabase',
-    host: 'localhost',
+    user: 'root',
+    password: 'Butter000',
+    database: 'test_db',
+    host: '127.0.0.1',
     port: '3306'
 })
 
