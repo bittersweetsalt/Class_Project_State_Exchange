10f0f2414c3a217703fba6277ab87954491b5b98
Chris Jol <jol.chris2@gmail.com>
2019-12-04 11:27:10 -0800

    commiting changes for merge, almost no changes on local branch

--- App/db/connection.js
+++ App/db/connection.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
 
+// const db = mysql.createConnection({
+//     user: 'ubuntu',
+//     password: 'csc648.07',
+//     database: 'sfsuDatabase',
+//     host: 'localhost',
+//     port: '3000'
+// })
+
 const db = mysql.createConnection({
-    user: 'ubuntu',
-    password: 'csc648.07',
-    database: 'sfsuDatabase',
-    host: 'localhost',
-    port: '3000'
+    user: 'root',
+    password: 'Butter000',
+    database: 'test_db',
+    host: '127.0.0.1',
+    port: '3306'
 })
 
 module.exports = db;
