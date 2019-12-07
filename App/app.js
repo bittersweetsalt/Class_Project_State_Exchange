dbbaaad587047d543994b57ea82ef9e27b734dd6
Kevin Venegas <kevinv234@gmail.com>
2019-12-07 15:45:31 -0800

    deleted a catch error function
    this function was creating errors

--- App/app.js
+++ App/app.js
@@ -85,12 +85,6 @@ app.use('/messaging', passport.authenticate('jwt', {session: false}), messagingR
 app.use('/messaging-index', passport.authenticate('jwt', {session: false}), messagesIndexRouter);
 
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
